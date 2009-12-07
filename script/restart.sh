d6cfb995f3086c73bc0b8c3ee6020976699e8ebd
Shannon Rush <shannonmrush@shannon-rushs-macbook.local>
2009-12-07 13:34:20 -0700

    still working on restart shell script

--- script/restart.sh
+++ script/restart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 cd /home/webuser/shannonline/current/
-/usr/bin/mongrel_rails cluster::start -C /etc/mongrel_cluster/mongrel_cluster.yml
+/usr/bin/mongrel_rails cluster::start -C config/mongrel_cluster.yml
 
