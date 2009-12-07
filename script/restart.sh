73cba96e488382a8112fa89530daccf0f4e45da5
Shannon Rush <shannonmrush@shannon-rushs-macbook.local>
2009-12-07 13:37:22 -0700

    restart shell script

--- script/restart.sh
+++ script/restart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
 cd /home/webuser/shannonline/current/
-/usr/bin/mongrel_rails cluster::start -C config/mongrel_cluster.yml
+/usr/bin/mongrel_rails cluster::configure -e production -p 3001 -N 1 -c /home/webuser/shannonline/current -a 127.0.0.1
+/usr/bin/mongrel_rails cluster::start
 
