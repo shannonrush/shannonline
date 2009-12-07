e12f95fdb2f6bae16fc1f54a30353968616caa81
Shannon Rush <shannonmrush@shannon-rushs-macbook.local>
2009-12-07 13:38:48 -0700

    changing from start to restart

--- script/restart.sh
+++ script/restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 cd /home/webuser/shannonline/current/
 /usr/bin/mongrel_rails cluster::configure -e production -p 3001 -N 1 -c /home/webuser/shannonline/current -a 127.0.0.1
-/usr/bin/mongrel_rails cluster::start
+/usr/bin/mongrel_rails cluster::restart
 
