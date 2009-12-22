bf72d07bfaf251854005fd165adb6d1467824778
Shannon Rush <shannonmrush@shannon-rushs-macbook.local>
2009-12-22 13:36:46 -0700

    removing db2s3

--- vendor/plugins/db2s3/spec/mysql_schema.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS people;
-CREATE TABLE people (
-name VARCHAR(255) NULL
-);
--- vendor/plugins/db2s3/spec/s3_config.example.rb
+++
@@ -1,5 +0,0 @@
-DB2S3::Config:: S3 = {
-  :access_key_id     => 'yourkey',
-  :secret_access_key => 'yoursecretkey',
-  :bucket            => 'db2s3_test'
-}
